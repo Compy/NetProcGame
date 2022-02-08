054ae50a11998def3c86dd9d9d57e42a091ced79
Dave <horsepin@protonmail.com>
2022-02-08 23:35:00 +0000

    added tests for pdbaddress

--- Tests/NetProc.Tests/PdbAddressConfigTests.cs
+++ Tests/NetProc.Tests/PdbAddressConfigTests.cs
@@ -5,11 +5,21 @@ namespace NetProc.Tests
 {
     public class PdbAddressConfigTests
     {
-        [Fact]
-        public void DecodePdbAddressTests()
+        [Theory]
+        [InlineData("A0:R0-B1-G2")]
+        public void DecodePdb_LED_AddressTests(string address)
         {
-            var address = PDBFunctions.DecodePdbAddress("A0:R0-B1-G2");
-            Assert.True(address.board == 0);
+            var decodedAddress = PDBFunctions.DecodePdbAddress(address);
+            Assert.True(decodedAddress.board == 0);
+        }
+
+        [Theory]
+        [InlineData("0/0/0")]//board 0, bankA (0) , output 0
+        [InlineData("0/1/0")]//board 0, bankB (1) , output 0
+        public void DecodePdb_COIL_AddressTests(string address)
+        {
+            var decodedAddress = PDBFunctions.DecodePdbAddress(address);
+            Assert.True(decodedAddress.board == 0);
         }
     }
 }
